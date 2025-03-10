0a5735a3f547f578b6cbd41d434b216fa9f4198f
Sanjana24kairamkonda <96222161+Sanjana24kairamkonda@users.noreply.github.com>
2025-03-10 15:05:53 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "project_id" {
+  description = "The GCP Project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP Region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "database_password" {
+  description = "Password for the Cloud SQL database user"
+  type        = string
+}
