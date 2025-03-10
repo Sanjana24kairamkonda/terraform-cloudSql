3f74ed631f909a51dc42dfb3cc868d797f60ce3a
Sanjana24kairamkonda <96222161+Sanjana24kairamkonda@users.noreply.github.com>
2025-03-10 15:39:29 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,3 +13,9 @@ variable "database_password" {
   description = "Password for the Cloud SQL database user"
   type        = string
 }
+
+variable "gcp_credentials_file" {
+  description = "Path to GCP credentials JSON file"
+  type        = string
+  default     = "terraform-sa-key.json"
+}
